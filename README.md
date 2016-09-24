21265ab3be2a175dceeac4a1429c05b1d736017b
Zarino Zappia <mail@zarino.co.uk>
2016-09-24 19:35:17 +0100

    Add an image for site by dragging file from desktop

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Then load the unpacked extension into the Google Chrome Extensions page, in deve
 ## How to display bookmarks in the Launchpad
 
 On first run, the extension will search for a “Launchpad” bookmarks folder, at the top level of the Bookmarks tree. If one is found, the child nodes are rendered as icons on the Launchpad. If no “Launchpad” bookmarks folder is found, an empty one is created. Just add your bookmarks to this folder, and they’ll appear on the Launchpad.
+
+To add an icon for a bookmark in Launchpad, drag an image file from your desktop onto the grey placeholder icon for a site. SVG images work best.
--- src/code.js
+++ src/code.js
@@ -23,35 +23,100 @@ var getLaunchpadSites = function getLaunchpadSites(cb){
     });
 }
 
+var createIconForSite = function createIconForSite(site, cb){
+    getImageForSite(site, function(dataURI){
+        var $icon = $('<span>').addClass('icon');
+
+        if(typeof dataURI !== 'undefined'){
+            $icon.css('background', 'transparent url(' + dataURI + ') no-repeat 0 0')
+        } else {
+            $icon.addClass('icon--placeholder')
+                .text(site.title.substr(0,1).toUpperCase());
+        }
+
+        return cb($icon);
+    });
+}
+
+var getImageForSite = function getImageForSite(site, cb){
+    chrome.storage.sync.get(site.id, function(result){
+        cb(result[site.id]);
+    });
+}
+
+var storeImageForSite = function storeImageForSite(dataURI, site, cb){
+    var data = {};
+    data[site.id] = dataURI;
+    chrome.storage.sync.set(data, cb);
+}
+
+var iconDragover = function iconDragover(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).addClass('icon--replace');
+}
+
+var iconDragleave = function iconDragleave(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).removeClass('icon--replace');
+}
+
+var iconDrop = function iconDrop(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).removeClass('icon--replace');
+
+    var site = $(this).parent().data('site');
+    var $icon = $(this);
+    var files = e.originalEvent.dataTransfer.files;
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function(){
+        storeImageForSite(reader.result, site, function(){
+            createIconForSite(site, function($newIcon){
+                $icon.replaceWith($newIcon);
+            });
+        });
+    }, false);
+
+    if(/^image[/]/.test(files[0].type)){
+        reader.readAsDataURL(files[0]);
+    }
+}
+
+var showHint = function showHint(){
+    var $a = $('<a>')
+        .on('click', function(){
+            chrome.tabs.create({ url: 'chrome://bookmarks' });
+        })
+        .attr('href', 'chrome://bookmarks')
+        .text('Open bookmark manager');
+    var $hint = $('<p>')
+        .addClass('hint')
+        .text('Add sites to the “Launchpad” bookmarks folder, and they will appear on this page.')
+        .append($a)
+        .appendTo('body');
+}
+
 getLaunchpadSites(function(sites){
     if(sites.length == 0){
-        var $a = $('<a>')
-            .on('click', function(){
-                chrome.tabs.create({ url: 'chrome://bookmarks' });
-            })
-            .attr('href', 'chrome://bookmarks')
-            .text('Open bookmark manager');
-        var $hint = $('<p>')
-            .addClass('hint')
-            .text('Add sites to the “Launchpad” bookmarks folder, and they will appear on this page.')
-            .append($a)
-            .appendTo('body');
+        showHint();
 
     } else {
         sites.forEach(function(site){
-            var $icon = $('<span>')
-                .addClass('icon')
-                .addClass('icon--placeholder')
-                .text(site.title.substr(0,1).toUpperCase());
-            var $label = $('<span>')
-                .addClass('label')
-                .text(site.title);
-            $('<a>')
-                .addClass('site')
-                .attr('href', site.url)
-                .append($icon)
-                .append($label)
-                .appendTo('body');
+            createIconForSite(site, function($icon){
+                $('<a>')
+                    .addClass('site')
+                    .attr('href', site.url)
+                    .text(site.title)
+                    .prepend($icon)
+                    .data('site', site)
+                    .on('dragover', '.icon', iconDragover)
+                    .on('dragleave', '.icon', iconDragleave)
+                    .on('drop', '.icon', iconDrop)
+                    .appendTo('body');
+            });
         });
     }
 });

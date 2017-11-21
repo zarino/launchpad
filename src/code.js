2a1d218d97fcf46a23df51e92b92dea42f720968
Zarino Zappia <mail@zarino.co.uk>
2017-11-21 21:25:36 +0000

    WIP: First attempt at cross-brower extension
    Abstracting the API out (Chrome uses callbacks, Firefox uses promises)
    worked well, and it wasn’t too hard to work around the different API
    response formats (eg: how the two browsers name their bookmark menus).
    
    But, there are irreconcilable differences between how the two browsers
    expect their manifest.json to be structured. For `storage.sync` to work
    in Firefox, there must be an `applications: {}` item in manifest.json,
    but Chrome will not accept an extension with `applications` in its
    manifest. So we’re at a stalemate.
    
    I’m starting to think, if we need two separate manifest.json files, we
    might as well keep it simple and have two completely separate `src`
    directories too. Some code will end up duplicated between the two `src`
    directories but at least we’ll gain some flexibility in how the code
    and styles can be tweaked to suit the destination browser.

--- src/code.js
+++ src/code.js
@@ -1,20 +1,57 @@
 var launchpadFolderName = 'Launchpad';
+var launchpadFolderParentId = '1';
+
+// In Firefox, the browser API is in the `browser` variable;
+// in Chrome, it’s in `chrome`. Even though Firefox includes its
+// own chrome->browser alias, we define our own here, to avoid
+// unexpected "not defined" errors when accessing grandparent
+// methods of the `chrome` alias in Firefox.
+var api = (typeof browser !== 'undefined') ? browser : chrome;
+
+// Work out whether we need to use a callback-based API
+// (Chrome), or a promise-based API (Firefox).
+var isCallbackAPI = (typeof browser === undefined);
+
+// Compatibility layer, to convert promise-based WebExtension-style
+// API methods into callback-based Chrome-style API methods.
+// eg: call(api.some.api.method, [arg1, arg2], callbackFn);
+var call = function call(method, args, cb) {
+    var args = args || [];
+    if (isCallbackAPI) {
+        args.push(cb);
+        method.apply(api, args);
+    } else {
+        var _promise = method.apply(api, args);
+        _promise.then(cb, function(err){ console.error(err); });
+    }
+}
+
+var getLaunchpadFolderParentId = function getLaunchpadFolderParentId(cb){
+    call(api.bookmarks.getTree, [], function(tree){
+        var parentFolder = tree[0]['children'].find(function(f){
+            // Ridiculously, Firefox gives its top level "Bookmarks Menu"
+            // an id of "menu________". In Chrome, it’s just "1".
+            return f.id === 'menu________' || f.id === '1';
+        });
+        cb(parentFolder.id);
+    });
+}
 
 var getLaunchpadSites = function getLaunchpadSites(cb){
-    chrome.bookmarks.getChildren('1', function(results){
+    call(api.bookmarks.getChildren, [launchpadFolderParentId], function(results){
         var launchPadFolder = results.find(function(result){
             return result.title === launchpadFolderName;
         });
 
         if(typeof launchPadFolder === 'undefined'){
-            chrome.bookmarks.create({
-                parentId: "1",
+            call(api.bookmarks.create, [{
+                parentId: launchpadFolderParentId,
                 title: launchpadFolderName
-            });
+            }]);
             return cb([]);
         }
 
-        chrome.bookmarks.getChildren(launchPadFolder['id'], function(results){
+        call(api.bookmarks.getChildren, [launchPadFolder['id']], function(results){
             if(results.length === 0){
                 return cb([]);
             }
@@ -39,7 +76,7 @@ var createIconForSite = function createIconForSite(site, cb){
 }
 
 var getImageForSite = function getImageForSite(site, cb){
-    chrome.storage.sync.get(site.id, function(result){
+    call(api.storage.sync.get, [site.id], function(result){
         cb(result[site.id]);
     });
 }
@@ -47,7 +84,7 @@ var getImageForSite = function getImageForSite(site, cb){
 var storeImageForSite = function storeImageForSite(dataURI, site, cb){
     var data = {};
     data[site.id] = dataURI;
-    chrome.storage.sync.set(data, cb);
+    call(api.storage.sync.set, [data], cb);
 }
 
 var iconDragover = function iconDragover(e){
@@ -86,37 +123,39 @@ var iconDrop = function iconDrop(e){
 }
 
 var showHint = function showHint(){
-    var $a = $('<a>')
-        .on('click', function(){
-            chrome.tabs.create({ url: 'chrome://bookmarks' });
-        })
-        .attr('href', 'chrome://bookmarks')
-        .text('Open bookmark manager');
     var $hint = $('<p>')
         .addClass('hint')
         .text('Add sites to the “Launchpad” bookmarks folder, and they will appear on this page.')
-        .append($a)
         .appendTo('body');
 }
 
-getLaunchpadSites(function(sites){
-    if(sites.length == 0){
-        showHint();
 
-    } else {
-        sites.forEach(function(site){
-            createIconForSite(site, function($icon){
-                $('<a>')
-                    .addClass('site')
-                    .attr('href', site.url)
-                    .text(site.title)
-                    .prepend($icon)
-                    .data('site', site)
-                    .on('dragover', '.icon', iconDragover)
-                    .on('dragleave', '.icon', iconDragleave)
-                    .on('drop', '.icon', iconDrop)
-                    .appendTo('body');
+getLaunchpadFolderParentId(function(id){
+
+    launchpadFolderParentId = id;
+
+    getLaunchpadSites(function(sites){
+        if(sites.length == 0){
+            showHint();
+
+        } else {
+            sites.forEach(function(site){
+                createIconForSite(site, function($icon){
+                    $('<a>')
+                        .addClass('site')
+                        .attr('href', site.url)
+                        .text(site.title)
+                        .prepend($icon)
+                        .data('site', site)
+                        .on('dragover', '.icon', iconDragover)
+                        .on('dragleave', '.icon', iconDragleave)
+                        .on('drop', '.icon', iconDrop)
+                        .appendTo('body');
+                });
             });
-        });
-    }
+        }
+    });
 });
+
+// var _promise = browser.bookmarks.getTree.apply(browser, []);
+// _promise.then(function(results){ console.log(results); }, function(err){ console.warn(err); });
